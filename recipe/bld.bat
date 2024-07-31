dbf53da314974ffca238ece773b5246790582b3b
Howard Butler <hobu.inc@gmail.com>
2024-07-31 09:51:48 -0500

    enable PROTOC compiler (#2)
    * enable PROTOC compiler
    
    * point to protoc for cross compilation scenarios
    
    * point to location of protoc compiler
    
    * conditionalize cross compilation scenario
    
    * need libprotobuf for build too?

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,7 +7,7 @@ cmake -G "Ninja" ^
       %CMAKE_ARGS% ^
       -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_BUILD_TYPE:STRING=Release ^
-      -DBUILD_PROTOC=OFF ^
+      -DBUILD_PROTOC=ON ^
       -DBUILD_SHARED_LIBS=ON ^
       ..
 
